0bf6e661d1a8451b8d99f9806bf2791051cf9dbb
Michal-python <mmmmm78h@gmail.com>
2024-09-14 12:12:50 +0200

    Changed server port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   plugins: [
     vue(),
     Components({
